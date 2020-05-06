315e8e87d8d2ece6eebbee6f11e8f923d390ad92
Giorgi Shakiashvili <giorgishakiashvili@gmail.com>
2020-05-06 23:57:58 +0400

    posters fixed, comments added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-
+//Create a default function (without a name) to export as a component
 export default function (fetchedData) {
+    //Destructuring the variable "fetchData", which represents to be the data object fetched from Contentful API
     const {imgSrc, title, releaseDate, director, cast, synopsis,} = fetchedData;
   return (
+    //Card structure is displayed from here
     <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
       <div class="flipper">
         <div class="front">
-          <img src={imgSrc} alt="film image"/>
+          <img src={imgSrc} alt="filmPoster"/>
         </div>
         <div id="color" class="back">
           <p>
