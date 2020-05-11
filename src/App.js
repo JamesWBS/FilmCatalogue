32c74aba888bfd62631276fc84e6f33160a795a9
Your Name <you@example.com>
2020-05-11 21:40:29 +0200

    added FilmFlix v2 front end page updates
    added FilmFlix v2 front end page updates

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "./navbar.css";
-import Home from "./views/home";
-import Showcase from "./views/showcase";
-import Movie from "./views/movie";
-import Search from "./components/search";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-export default () => {
-  return (
-    <Router>
-      <div>
-        <div class="sidenav">
-          <a href="#">
-            <Link to="/">Home</Link>
-          </a>
-          <a href="#">
-            {" "}
-            <Link to="/showcase">Showcase</Link>
-          </a>
-          <a href="#">
-            {" "}
-            <Link to="/movies">Movies</Link>
-          </a>
-          <a href="#">
-            {" "}
-            <Link to="/search">Search</Link>
-          </a>
-        </div>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. Switch and route defines what happens when go on path */}
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/showcase">
-            <Showcase></Showcase>
-          </Route>
-          <Route path="/movies">
-            <Movie></Movie>
-          </Route>
-          <Route path="/search">
-            <Search></Search>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+class App extends Component {
+    constructor(props) { // this constructor initializes the default state
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {     // inserting this method callAPI() will fetch the data from the API 
+        // and store the response on this.state.apiResponse
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    componentDidMount() {   // inserting this react lifecycle method called
+        // componentDidMount(), that will execute the callAPI() method
+        // after the component mounts
+        this.callAPI();
+    }
+
+    // display a paragraph on our client page, with text retrieved from the API
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Film Search</h1>
+                </header>
+                <p className="App-intro">{this.state.apiResponse}</p>
+            </div>
+        );
+    }
+}
+
+export default App;
