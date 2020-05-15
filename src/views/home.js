d9507097faf136e29dadb7e1b3ea9ad3a2309d99
Your Name <you@example.com>
2020-05-15 10:14:29 +0200

    css 2 update
    update 2

--- src/views/home.js
+++ src/views/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../index.css";
 
 export default function Home() {
 
@@ -6,6 +7,20 @@ export default function Home() {
     <div id="color" class="main">
       {" "}
       Hello this is home path
+      <div>WE LOVE MOVIES</div>
+      <br></br>
+      <br></br>
+      <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+      </div>
     </div>
   );
 }
